0121367be65e42c8cdb34b8f6a4e4dd3e9b0d1fe
Dominic <dominicbonar@outlook.com>
2021-08-31 21:35:46 -0700

    webpack dev server runs

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const HMR = require('fastify-webpack-hmr')
-const fastify = require('fastify')()
+const fastify = require('fastify')({
+    logger: true
+})
 const { join } = require('path')
 
 const config = join(__dirname, './webpack.config.js')
 
 fastify.register(HMR, { config })
 
-fastify.get('/*', (request, reply) => {
-	reply.send({ hello: 'world' })
+fastify.get('/', async (request, reply) => {
+	return { hello: 'world' }
 })
 
 const start = async () => {
     try {
         await fastify.listen(3000)
     } catch (error) {
-        await fastify.log(error)
+        await fastify.logger(error)
         process.exit(1)
     }
 }
